c5a7ff116425e997a1274fdbdd680d465b7d8502
Mikehope254 <mikehope1738@gmail.com>
2025-03-06 01:29:07 +0300

    ES6 module updated

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -1,12 +1,12 @@
-module.exports.notFound = (req, res, next) => {
+export function notFound(req, res, next) {
   const err = new Error("Not Found");
   err.status = 404;
 
   next(err);
-};
+}
 
-module.exports.errors = (err, req, res, next) => {
+export function errors(err, req, res, next) {
   res.status(err.status || 500).json({
     err: err.message || "Something went wrong",
   });
-};
+}
--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-require("dotenv").config();
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import { json } from "body-parser";
+import bodyParser from "body-parser";
 
-import { notFound, errors } from "./handlers";
+import { notFound, errors } from "./handlers/index.js";
 
+dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
-app.use(json());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json({ hello: "world" });
