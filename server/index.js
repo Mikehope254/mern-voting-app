3c0564718b45b6846dd19d9bab3eee5e5af212db
Mikehope254 <mikehope1738@gmail.com>
2025-03-06 15:09:30 +0300

    Database connected .env port added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
+import db from "./models/index.js";
 import { notFound, errors } from "./handlers/index.js";
 
-dotenv.config();
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
---
+++ server/models/index.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+mongoose.set("debug", true);
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb://localhost/vote")
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+export default mongoose;
