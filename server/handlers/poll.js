536fc50867c7818c9aa6eed480c2edfeecf185c8
Mikehope254 <mikehope1738@gmail.com>
2025-03-12 12:51:14 +0300

    Poll routes updated

--- server/handlers/poll.js
+++ server/handlers/poll.js
@@ -1,4 +1,4 @@
-import { Poll } from "../models/index.js";
+import { Poll, User } from "../models/index.js";
 
 export async function showPolls(req, res, next) {
   try {
@@ -12,13 +12,20 @@ export async function showPolls(req, res, next) {
 
 export async function createPoll(req, res, next) {
   try {
+    console.log(req.decoded);
+    const { id } = req.decoded;
+    const user = await User.findById(id);
+
     const { question, options } = req.body;
     const poll = await Poll.create({
       question,
+      user,
       options: options.map((option) => ({ option, votes: 0 })),
     });
+    user.polls.push(poll._id);
+    await user.save();
 
-    res.status(201).json(poll);
+    res.status(201).json({ ...poll._doc, user: user._id });
   } catch (error) {
     error.status = 400;
     next(error);
--- server/routes/poll.js
+++ server/routes/poll.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { showPolls, createPoll } from "../handlers/poll.js";
-// import authMiddleware from "../middlewares/auth.js";
+import authMiddleware from "../middlewares/auth.js";
 
 const router = express.Router();
 router.route("/").get(showPolls); //Show everything
-router.route("/").post(createPoll);
+router.route("/").post(authMiddleware, createPoll);
 
 export default router;
