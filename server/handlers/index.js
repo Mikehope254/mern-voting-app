b0eed72e5ce4e565e7799643571f99778eb70ef8
Mikehope254 <mikehope1738@gmail.com>
2025-03-13 19:27:02 +0300

    Chnaged err to message in handler/index.js

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -12,6 +12,6 @@ export function notFound(req, res, next) {
 
 export function errors(err, req, res, next) {
   res.status(err.status || 400).json({
-    err: err.message || "Something went wrong",
+    message: err.message || "Something went wrong",
   });
 }
