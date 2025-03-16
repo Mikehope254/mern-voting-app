3fe05ee759f868194f9c7b373431bde32e559f97
Mikehope254 <mikehope1738@gmail.com>
2025-03-16 21:39:33 +0300

    Finished Setup of API services

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import api from "../services/api";
 
-function App() {
-  return (
-    <div>
-      <h1>App Works</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <h1>App Works</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  async componentDidMount() {
+    const result = await api.call("post", "auth/login", {
+      username: "username",
+      password: "password",
+    });
+
+    console.log(result);
+  }
+
+  render() {
+    return <div>App works</div>;
+  }
 }
-
 export default App;
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const host = "http://localhost:4000/api";
+
 export const setToken = (token) => {
   if (token) {
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
@@ -9,7 +11,7 @@ export const setToken = (token) => {
 };
 
 export const call = async (method, path, data) => {
-  const response = await axios[method](`/${path}`, data);
+  const response = await axios[method](`${host}/${path}`, data);
   return response;
 };
 
