7d0012433d1596ad9fd812d55b667e556b066844
Mikehope254 <mikehope1738@gmail.com>
2025-03-20 19:57:12 +0300

    Provider ,store added

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import api from "../services/api";
+import store from "../store";
 
 const App = () => (
   <Provider store={store}>
