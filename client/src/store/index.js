6159f490f74d03b2e7deeab53cc49e4b6b4ea216
Mikehope254 <mikehope1738@gmail.com>
2025-03-26 23:03:25 +0300

    Fixed Errors by reverting to deprecated createStore instead of configureStore

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
+//import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducers/index.js";
 
 const DEFAULT_STATE = {
   error: { message: null },
 };
 
-export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(thunk.default),
-  devTools: process.env.NODE_ENV !== "production",
-});
+// export const store = configureStore({
+//   reducer: rootReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(thunk.default),
+//   devTools: process.env.NODE_ENV !== "production",
+// });
+
+export const store = createStore(
+  rootReducer,
+  DEFAULT_STATE,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
