fdbf3b40b8fef57ba1e15d5b335f03b0ee6911fe
Mikehope254 <mikehope1738@gmail.com>
2025-05-12 09:58:08 +0300

    readme modified

--- server/models/index.js
+++ server/models/index.js
@@ -1,30 +1,20 @@
-const mongoose = require('mongoose');
-// mongoose.set('debug', true);
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.DATABASE);
+const mongoose = require("mongoose");
 
-// module.exports.User = require('./user');
-// module.exports.Poll = require('./poll');
-
-// import mongoose from "mongoose";
-// import User from "./user.js";
-// import Poll from "./polls.js";
-
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 mongoose.Promise = global.Promise;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost/vote', {
+    await mongoose.connect("mongodb://localhost/vote", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected Successfully');
+    console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
 module.exports.connectDB = connectDB;
-module.exports.User = require('./user');
-module.exports.Poll = require('./poll');
+module.exports.User = require("./user");
+module.exports.Poll = require("./poll");
